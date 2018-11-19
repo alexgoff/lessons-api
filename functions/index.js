d4a8bd42ef5a2e26c68fe62eb10183b7ef733557
Alex Goff <alexander.r.goff@gmail.com>
2018-11-18 22:55:22 -0500

    add cross origin headers

--- functions/index.js
+++ functions/index.js
@@ -1,8 +1,12 @@
 const functions = require('firebase-functions');
 const express = require('express'); // call up express
+const cors = require('cors');
 const app = express(); // define app as express
 const bodyParser = require('body-parser'); // bring in body parse
 const channelJson = require('./api/data/channel.json'); //import our JSON 
+
+// Automatically allow cross-origin requests
+app.use(cors({ origin: true }));
 
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(bodyParser.json());
